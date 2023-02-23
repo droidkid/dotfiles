10a35bf7c7d5b8e558b42960d0babb4008c03ea2
Yuvaraj Chesetti <yuvaraj.chesetti@gmail.com>
2023-02-22 17:50:11 -0700

    Removing scripts from .cfg

--- .cfg/README.md
+++
@@ -1,5 +0,0 @@
-## Setting up on a new machine
-
-```bash
-echo "alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'" >> $HOME/.bashrc
-```
--- .cfg/setup_vim.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir -p ~/.vim/pack/vendor/start
-git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
-git clone https://github.com/ctrlpvim/ctrlp.vim ~/.vim/pack/vendor/start/ctrlp
-
